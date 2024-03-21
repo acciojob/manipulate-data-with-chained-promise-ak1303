f0a3cd3f7ed645599297f214a686e35fa8c448bb
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-21 22:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ function multiplyBy2(num){
 	return num*2;
 }
 let promise = new Promise((resolve,reject)=>{
-	setTimeout(()=>resolve(arr),1000);
+	setTimeout(()=>resolve(arr),3000);
 })
 promise.then((data)=>{
-	let nums = data.filter(num=>even(num));
-	let text=nums.join(", ");
-	output.innerHTML = text;
+	let nums = data.filter(num=>even(num));  
 	return new Promise((res,rej)=>{
-		setTimeout(()=>res(nums),2000);
+		setTimeout(()=>res(nums),1000); 
 	})
-}).then((data)=>{
-		let nums = data.map(num=>multiplyBy2(num));
-		let text = nums.join(", ");
+}).then((nums)=>{
+		let text=nums.join(", "); 
 		output.innerHTML = text;
+		nums = nums.map(num=>multiplyBy2(num));
+		return new Promise((res,rej)=>{
+			setTimeout(()=>res(nums),1000);
+			})
+}).then((nums)=>{
+	let text = nums.join(", ");
+	output.innerHTML = text;
 })
